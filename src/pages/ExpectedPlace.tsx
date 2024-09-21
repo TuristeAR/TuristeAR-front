810862734d15170d68c9190c433ebd17c021612a
MedinaDomingo <98916526+MedinaDomingo@users.noreply.github.com>
2024-09-21 18:31:56 -0300

    update article card

--- src/pages/ExpectedPlace.tsx
+++ src/pages/ExpectedPlace.tsx
@@ -139,7 +139,7 @@ const ExpectedPlace = () => {
                     </div>
                 </div>
             </section>
-            
+
             {/* Posts usuarios */}
 
             <section>
@@ -195,10 +195,67 @@ const ExpectedPlace = () => {
                     <h3 className="text-4xl font-bold">Cultura y tradiciones</h3>
                     <hr />
                     <div className="flex gap-2 mt-5 justify-around flex-wrap">
+                        <section className="bg-white">
+                            <div className="gap-16 items-center  px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2  lg:px-6">
+                                <div className="font-light text-gray-500 sm:text-lg">
+                                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Idioma, Religion y Población</h2>
+                                    <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+                                    <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+                                </div>
+                                <div className="grid grid-cols-2 gap-4 mt-8">
+                                    <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+                                    <img className="mt-4 w-full  rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
+                                </div>
+                            </div>
+                            <div className="gap-16 items-center px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2  lg:px-6">
+                               <div className="font-light text-gray-500 sm:text-lg">
+                                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Ropa Tradicional</h2>
+                                    <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+                                    <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+                                </div>
+                                <div className="grid grid-cols-2 gap-4 mt-8">
+                                    <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+                                    <img className="mt-4 w-full  rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
+                                </div>
+                                
+                            </div>
+                        </section>
 
                     </div>
                     <div className="text-center mt-10">
-                        <button className="btn-blue">Ver Puntos de interes</button>
+                        <button className="btn-blue">Ver más información</button>
+                    </div>
+                </div>
+            </section>
+
+            {/* Gastronomia */}
+
+            <section>
+                <div className="w-10/12 m-auto mt-20">
+                    <h3 className="text-4xl font-bold">Cultura y tradiciones</h3>
+                    <hr />
+                    <div className="flex gap-2 mt-5 justify-around flex-wrap">
+                        <section className="bg-white">
+                            <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+                                
+                            </div>
+                            <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+                               
+                                <div className="grid grid-cols-2 gap-4 mt-8">
+                                    <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+                                    <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
+                                </div>
+                                <div className="font-light text-gray-500 sm:text-lg">
+                                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Ropa Tradicional</h2>
+                                    <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+                                    <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+                                </div>
+                            </div>
+                        </section>
+
+                    </div>
+                    <div className="text-center mt-10">
+                        <button className="btn-blue">Ver más información</button>
                     </div>
                 </div>
             </section>
