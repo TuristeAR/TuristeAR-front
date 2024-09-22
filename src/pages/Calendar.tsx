335529cfacfd8c415dcb73e2cafe65316e5517cc
GonzaSalda <gonza.salda00@gmail.com>
2024-09-22 19:23:32 -0300

    Refactor Calendar component and add deleteEvent function

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -18,19 +18,23 @@ const Calendar = () => {
     }
   };
 
-  const handleDateClick = (arg: { dateStr: any; }) => {
+  const handleDateClick = (arg: { dateStr: any }) => {
     const title = prompt('Introduce el título del nuevo evento:');
     if (title) {
       setEvents([...events, { id: String(events.length + 1), title, date: arg.dateStr }]);
     }
   };
+
+  const deleteEvent = (id: string) => {
+    setEvents(events.filter((event) => event.id !== id));
+  };
   return (
-    <section className="h-screen xl:h-[1600px] overflow-x-clip relative">
+    <section className="h-screen xl:h-auto overflow-x-clip relative">
       <Header containerStyles="bg-primary fixed top-0 left-0 right-0 z-[60]" />
-      <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-4 h-full mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-[250px_1fr] gap-4 h-full mt-20">
         {/* Left Column */}
         <aside className="col-span-1 p-4 hidden md:flex">
-          <div className="flex flex-col h-full">
+          <div className="flex flex-col h-full w-full">
             <div className="flex flex-col justify-center border-b border-gray">
               <h2 className="font-semibold tracking-[-0.5px] leading-none">Categorías seguidas</h2>
               <div className="flex flex-col p-2 gap-y-2">
@@ -59,25 +63,36 @@ const Calendar = () => {
 
             {/* Eliminar actividad */}
             <div className="flex flex-col gap-4 my-4">
-              <h2 className="font-semibold tracking-[-0.5px] leading-none">Eliminar actividades</h2>
-              <div className="w-full flex flex-col gap-2 mb-2">
-                {['Museo de las bellas artes', 'Teatro Colón', 'Tour por Palermo'].map(
-                  (activity) => (
-                    <div key={activity} className="flex items-center w-full gap-2">
-                      <div className="bg-gray-50 w-8 h-8 rounded-full"></div>
-                      <p>{activity}</p>
-                    </div>
-                  ),
-                )}
-              </div>
-              <div className="p-2">
-                <p>Descubrir más</p>
-              </div>
+              {events.length === 0 ? (
+                <p>No hay actividades para eliminar</p>
+              ) : (
+                <>
+                  <h2 className="font-semibold tracking-[-0.5px] leading-none">
+                    Eliminar actividades
+                  </h2>
+                  <div className="w-full flex flex-col gap-2 mb-2">
+                    {events.map((activity, index) => (
+                      <div key={index} className="flex items-center w-full gap-2">
+                        <button
+                          onClick={() => deleteEvent(activity.id)}
+                          className="bg-gray-50 w-8 h-8 rounded-full"
+                        ></button>
+                        <p>{activity.title}</p>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="p-2">
+                    <p>Descubrir más</p>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </aside>
-        <main className="col-span-1 container mx-auto flex justify-center flex-1">
-          <div className="flex flex-col h-full mx-4 md:mx-0 md:w-[700px]">
+
+        {/* Main Column */}
+        <main className="col-span-1 container mx-auto flex justify-center flex-1 my-4">
+          <div className="flex flex-col h-full mx-4 md:mx-0 md:w-[900px]">
             <FullCalendar
               plugins={[dayGridPlugin, interactionPlugin]}
               initialView="dayGridMonth"
