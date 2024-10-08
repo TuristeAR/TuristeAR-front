4b8c3c6836c4f611218209ff911743cb4ed55f6b
Medina <98916526+MedinaDomingo@users.noreply.github.com>
2024-10-08 17:08:10 -0300

    Update Destinations.tsx

--- src/pages/Destinations.tsx
+++ src/pages/Destinations.tsx
@@ -5,14 +5,15 @@ import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { get } from '../utilities/http.util';
 
-interface Province {
+type Province = {
   id: number;
   name: string;
   description: string;
   images: string[];
+  places: Place[]
 }
 
-const usuariosReview = [
+const reviews = [
   {
     imgPerson: '/assets/person.svg',
     usuario: 'Pablo Ramirez',
@@ -75,10 +76,39 @@ const usuariosReview = [
   },
 ];
 
+interface Review {
+  id: number;
+  createdAt: string;
+  publishedTime: string;
+  rating: number;
+  text: string;
+  authorName: string;
+  authorPhoto: string;
+  photos: string[];
+}
+
+interface Place {
+  id: number;
+  createdAt: string;
+  googleId: string;
+  name: string;
+  types: string[];
+  address: string;
+  latitude: number;
+  longitude: number;
+  rating: number;
+  openingHours: string[];
+  phoneNumber: string | null;
+  reviews: Review[];
+}
+
+
+
 const Destinations = () => {
   const navigate = useNavigate();
   const sectionRef = useRef<HTMLDivElement>(null);
   const [provinces, setProvinces] = useState<Province[]>([]);
+  const [reviews, setReviews] = useState<Province>(); 
 
   const scrollToSection = () => {
     if (sectionRef.current) {
@@ -108,6 +138,24 @@ const Destinations = () => {
 
     fetchProvinces();
   }, []);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      if (selectedProvince) {
+        try {
+          const response = await get(`https://api-turistear.koyeb.app/provinces/${selectedProvince.id}`, {
+            'Content-Type': 'application/json',
+          });
+          console.log(response)
+          setReviews(response);
+        } catch (error) {
+          console.error('Error fetching reviews:', error);
+        }
+      }
+    };
+
+    fetchReviews();
+  }, [selectedProvince]);
 
   const handleProvinceClick = (id: number) => {
     const province = provinces.find((p) => p.id === id);
@@ -171,30 +219,30 @@ const Destinations = () => {
             </div>
             <div className="border-b border-gray my-4 "></div>
             <Carousel>
-              {usuariosReview.map((item, index) => {
+              {reviews?.places.map((item, index) => {
                 return (
                   <div className="flex flex-col gap-y-4" key={index}>
                     <div className="flex justify-between items-center px-2 text-gray">
                       <div className="flex items-center gap-4  ">
                         <div className="rounded-full p-2 border border-1 border-black">
-                          <img className="w-8 h-8" src={item.imgPerson} alt="person" />
+                          <img className="w-8 h-8" src={item.reviews[0].authorPhoto} alt="person" />
                         </div>
-                        <p>{item.usuario}</p>
+                        <p>{item.reviews[0].authorName}</p>
                       </div>
-                      <p>{item.fecha}</p>
+                      <p>{item.reviews[0].publishedTime}</p>
                     </div>
                     <p className="font-light text-gray-500 text-sm md:text-base lg:text-lg text-start">
-                      {item.description}
+                      {item.reviews[0].text}
                     </p>
                     <div className="flex justify-center gap-2">
-                      {item.img.map((image, index) => (
+                      {item.reviews[0].photos.map((image, index) => (
                         <div
                           key={index}
                           className="w-[150px] h-[100px] md:w-[200px] md:h-[170px] overflow-hidden"
                         >
                           <img
-                            key={image.id}
-                            src={image.src}
+                            key={index}
+                            src={image}
                             className="w-full h-full object-cover"
                             alt={selectedProvince?.name}
                           />
