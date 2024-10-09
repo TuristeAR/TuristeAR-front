8d8b3d9632933fb9637ef563f57c342e5d46c904
Paolo Aleman <paoloaleman86@gmail.com>
2024-10-08 22:55:56 -0300

    Publicaciones extraidas de la base de datos, jobs y forum solucionados y menu resaltado en LeftCommunity

--- src/components/Community/LeftCommunity.tsx
+++ src/components/Community/LeftCommunity.tsx
@@ -1,4 +1,5 @@
 import { CommunityFilters } from './CommunityFilters';
+import { useState } from 'react';
 const infoCategories = [
   {
     title: 'Filtrar por categoría',
@@ -14,20 +15,23 @@ const infoCategories = [
   },
 ];
 
-export const LeftCommunity = () => {
+
+
+export const LeftCommunity = (props:{vista : string}) => {
+  const {vista}=props;
   return (
     <>
       <div className="flex flex-col lg:w-[20%] w-[100%] lg:block hidden p-6 gap-6 border-r border-[#999999]">
         <div className="flex flex-col gap-4 text-l font-semibold">
-          <div className="flex flex-row items-center gap-2 hover:bg-[#d9d9d9] rounded-xl">
+          <div className={`flex flex-row items-center gap-2 hover:bg-[#d9d9d9] rounded-xl py-2 px-4 ${(vista=='publications') ? 'bg-[#c0daeb]' : ''}`}>
             <img src="/assets/home.svg" alt="Publicaciones" className="w-[25px]" />
             <a href="/publications">Publicaciones</a>
           </div>
-          <div className="flex flex-row items-center gap-2 hover:bg-[#d9d9d9] rounded-xl">
+          <div className={`flex flex-row items-center gap-2 hover:bg-[#d9d9d9] rounded-xl  py-2 px-4 ${(vista=='forum') ? 'bg-[#c0daeb]' : ''}`}>
             <img src="/assets/message.svg" alt="Foro y preguntas" className="w-[25px]" />
             <a href="/forum">Foro y preguntas</a>
           </div>
-          <div className="flex flex-row items-center gap-2 hover:bg-[#d9d9d9] rounded-xl">
+          <div className={`flex flex-row items-center gap-2 hover:bg-[#d9d9d9] rounded-xl  py-2 px-4 ${(vista=='jobs') ? 'bg-[#c0daeb]' : ''}`}>
             <img src="/assets/personBlue.svg" alt="Ofertas de trabajo" className="w-[25px]" />
             <a href="/jobs">Ofertas de trabajo</a>
           </div>
