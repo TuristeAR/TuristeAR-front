696131c14c6917aa960d5cee0c9aff29c80be7e2
GonzaSalda <gonza.salda00@gmail.com>
2024-10-11 22:46:19 -0300

    Add new event functionality to Calendar component

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+/* Full Calendar */
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import esLocale from '@fullcalendar/core/locales/es';
+import { AddActivityCalendar } from './AddEventCalendar';
+import { ModalActivity } from './ModalEvent';
 
 export const Calendar = ({
   activities,
@@ -11,30 +15,55 @@ export const Calendar = ({
   activities: any;
   setActivities: any;
 }) => {
+  const [openNewEvent, setOpenNewEvent] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(false);
+
+  const [selectedDate, setSelectedDate] = useState('');
+  const [eventName, setEventName] = useState('');
+
   const handleDateClick = (arg: { dateStr: string }) => {
-    const name = prompt('Introduce el título del nuevo evento:');
-    if (name) {
+    setSelectedDate(arg.dateStr); // Guardar la fecha seleccionada
+    setOpenNewEvent(true);
+  };
+
+  const handleEventClick = (info: any) => {
+    setSelectedDate(info.event);
+    setSelectedEvent(true);
+  };
+
+  const handleClose = () => {
+    setOpenNewEvent(false);
+    setSelectedEvent(false);
+  };
+
+  const handleSave = () => {
+    if (eventName) {
       const newId = String(Date.now());
       setActivities([
         ...activities,
-        { id: newId, name, fromDate: arg.dateStr, toDate: arg.dateStr },
+        { id: newId, name: eventName, fromDate: selectedDate, toDate: selectedDate },
       ]);
+      setOpenNewEvent(false);
+      setEventName('');
     }
   };
 
-  const deleteEvent = (id: string) => {
-    setActivities(activities.filter((event: any) => event.id !== id));
-  };
+  const deleteEvent = (id: number) => {
+    if (window.confirm('¿Estás seguro de que deseas eliminar esta actividad?')) {
+      console.log('Actividades antes:', activities);
 
-  const modifyEvento = (id: string) => {
-    const eventoSelect = activities.find((event: any) => event.id === id);
-    const name = prompt('Modificar evento:', eventoSelect?.name);
-    if (name) {
-      const updatedEvents = activities.map((event: any) =>
-        event.id === id ? { ...event, name } : event,
-      );
+      const updatedEvents = activities.filter((event: any) => event.id !== id);
       setActivities(updatedEvents);
+      console.log('Actividades después:', updatedEvents);
     }
+    setSelectedEvent(false);
+  };
+
+  const editEvent = (id: number, newName: string) => {
+    const updatedEvents = activities.map((event: any) =>
+      event.id === id ? { ...event, name: newName } : event,
+    );
+    setActivities(updatedEvents);
   };
 
   // Formatear actividades para FullCalendar
@@ -63,40 +92,55 @@ export const Calendar = ({
     });
   }, []);
 
+  
+
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      locale={esLocale}
-      events={formattedActivities}
-      dateClick={handleDateClick}
-      editable={true}
-      eventDrop={(info) => {
-        const updatedEvents = activities.map((event: any) =>
-          event.id === info.event.id ? { ...event, date: info.event.startStr } : event,
-        );
-        setActivities(updatedEvents);
-      }}
-      eventContent={(eventInfo) => (
-        <div className="flex flex-col overflow-hidden">
-          <span className="text-[11px] md:text-sm font-semibold whitespace-normal break-words]">
-            {eventInfo.event.title.replace(/ - \d{1,2} \w+\./, '')}
-          </span>
-          <div className="flex justify-between mt-1">
-            <button onClick={() => modifyEvento(eventInfo.event.id)} className="ml-2 text-blue-500">
-              ✏️
-            </button>
-            <button onClick={() => deleteEvent(eventInfo.event.id)} className="mr-2 text-blue-500">
-              ❌
-            </button>
-          </div>
-        </div>
+    <div className='relative'>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        locale={esLocale}
+        events={formattedActivities}
+        dateClick={handleDateClick}
+        eventClick={handleEventClick}
+        editable={true}
+        eventDrop={(info) => {
+          const updatedEvents = activities.map((event: any) =>
+            event.id === info.event.id ? { ...event, date: info.event.startStr } : event,
+          );
+          setActivities(updatedEvents);
+        }}
+        eventContent={(eventInfo) => (
+          <>
+            <div className="flex flex-col overflow-hidden">
+              <span className="text-[11px] md:text-sm font-semibold whitespace-normal break-words">
+                {eventInfo.event.title.replace(/ - \d{1,2} \w+\./, '')}
+              </span>
+              {selectedEvent && (
+                <ModalActivity
+                  handleClose={handleClose}
+                  editEvent={editEvent}
+                  deleteEvent={deleteEvent}
+                  eventInfo={eventInfo}
+                />
+              )}
+            </div>
+          </>
+        )}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,dayGridWeek,dayGridDay',
+        }}
+      />
+      {openNewEvent && (
+        <AddActivityCalendar
+          eventName={eventName}
+          setEventName={setEventName}
+          handleClose={handleClose}
+          handleSave={handleSave}
+        />
       )}
-      headerToolbar={{
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,dayGridWeek,dayGridDay',
-      }}
-    />
+    </div>
   );
 };
