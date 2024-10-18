741a1a8d3a5d20314e8342e1874d8096bd3c782c
TomasAyerbe <tomayerbe@gmail.com>
2024-10-18 09:04:55 -0300

    Update landing page

--- src/components/Landing/ScrollingText.tsx
+++ src/components/Landing/ScrollingText.tsx
@@ -2,14 +2,19 @@ import { motion } from 'framer-motion';
 
 export const ScrollingText = () => {
   return (
-    <div className="flex w-full  text-center overflow-hidden  py-5  text-[40px] md:text-[80px] bg-primary text-white">
+    <div className="flex w-full text-center overflow-hidden py-5 text-[40px] md:text-[80px] bg-primary text-white">
       <motion.div
         initial={{ x: 0 }}
         animate={{ x: '-100%' }}
         transition={{ duration: 15, repeat: Infinity, ease: 'linear' }}
-        className="whitespace-nowrap mr-12"
+        className="whitespace-nowrap"
       >
-        <p>VIAJÁ INTELIGENTE VIVÍ LA ARGENTINA</p>
+        <p className="inline-block mx-3">
+          VIAJÁ INTELIGENTE<span className="font-bold"> VIVÍ ARGENTINA </span>
+        </p>
+        <p className="inline-block mx-3">
+          VIAJÁ INTELIGENTE<span className="font-bold"> VIVÍ ARGENTINA </span>
+        </p>
       </motion.div>
       <motion.div
         initial={{ x: 0 }}
@@ -17,7 +22,12 @@ export const ScrollingText = () => {
         transition={{ duration: 15, repeat: Infinity, ease: 'linear' }}
         className="whitespace-nowrap"
       >
-        <p>VIAJÁ INTELIGENTE VIVÍ LA ARGENTINA</p>
+        <p className="inline-block mx-3">
+          VIAJÁ INTELIGENTE<span className="font-bold"> VIVÍ ARGENTINA </span>
+        </p>
+        <p className="inline-block mx-3">
+          VIAJÁ INTELIGENTE<span className="font-bold"> VIVÍ ARGENTINA </span>
+        </p>
       </motion.div>
     </div>
   );
