36acf7c944eb8f0c2ad4d1dc99f6a35f9ca86d38
Paolo Aleman <paoloaleman86@gmail.com>
2024-10-20 23:38:14 -0300

    Create forum and refactor type Forum

--- src/pages/Forum.tsx
+++ src/pages/Forum.tsx
@@ -4,55 +4,20 @@ import { ImageGallery } from '../components/ImageGallery/ImageGallery';
 import { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
 import logoAnimado from '../assets/logoAnimado.json';
-
-const infoComments = [
-  {
-    user: 'Camila Ibarra',
-    day: '25 sep 2024',
-    comment:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse corrupti laborum possimus' +
-      ' ad eligendi iusto, perferendis atque accusantium consequatur facere.',
-    imgPerson: '/assets/person.svg',
-    imgs: [],
-  },
-  {
-    user: 'Abigail Suriani',
-    day: '25 sep 2024',
-    comment:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse corrupti laborum possimus' +
-      ' ad eligendi iusto, perferendis atque accusantium consequatur facere.',
-    imgPerson: '/assets/person.svg',
-    imgs: [
-     '/assets/san-nicolas-buenos-aires.webp' ,
-     '/assets/san-nicolas-buenos-aires.webp' ,
-     '/assets/san-nicolas-buenos-aires.webp' ,
-    ],
-  },
-  {
-    user: 'Abigail Suriani',
-    day: '25 sep 2024',
-    comment:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse corrupti laborum possimus' +
-      ' ad eligendi iusto, perferendis atque accusantium consequatur facere.',
-    imgPerson: '/assets/person.svg',
-    imgs: [
-      '/assets/san-nicolas-buenos-aires.webp' ,
-      '/assets/san-nicolas-buenos-aires.webp' ,
-      '/assets/san-nicolas-buenos-aires.webp' ,
-    ],
-  },
-];
+import { CreateForums } from '../components/Community/CreateForums';
 
 const Forum = () => {
-  type Place = {
-    name: string;
+
+  type Category = {
+    id: number,
+    description: string
   };
 
   type Forum = {
     id: number;
     name: string;
     description: string;
-    place: Place | null;
+    category: Category | null;
   };
 
   const [categorySelected, setCategorySelected] = useState<number | null>(null);
@@ -80,8 +45,6 @@ const Forum = () => {
     fetchData();
   }, []);
 
-  const enterTheForum = () => {};
-
   return (
     <>
       {isLoading ?
@@ -100,8 +63,13 @@ const Forum = () => {
               setCategorySelected={setCategorySelected}
             />
             <div className="lg:w-[80%] w-[100%] lg:p-10 p-4 flex flex-col gap-10 overflow-scroll scrollbar-hidden">
+
+              <CreateForums />
+
               <div className={'grid lg:grid-cols-2 grid-cols-1 gap-6'}>
-                {forums?.map((forum, index) => (
+                {forums?.filter((forum)=> {
+                  return categorySelected == null || forum.category.id == categorySelected;
+                } ).map((forum, index) => (
                   <div
                     key={index}
                     className="w-[100%] mx-auto p-4 rounded-2xl shadow-[0_10px_25px_-10px_rgba(0,0,0,4)] flex flex-col gap-4"
@@ -111,7 +79,7 @@ const Forum = () => {
                     <div className={'flex justify-end'}>
                       <div
                         className="lg:btn-blue px-4 py-2 bg-primary hover:bg-primary-3 text-white rounded-2xl flex items-center justify-center">
-                        <a href={`/forum/${forum.id}`} onClick={enterTheForum}>
+                        <a href={`/forum/${forum.id}`}>
                           Ingresar
                         </a>
                       </div>
