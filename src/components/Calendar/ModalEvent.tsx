82222c9229b3ae66d6f2d3875713f36ee1f70fbf
TomasAyerbe <tomayerbe@gmail.com>
2024-10-27 23:30:09 -0300

    Update Modal Event

--- src/components/Calendar/ModalEvent.tsx
+++ src/components/Calendar/ModalEvent.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, Key, useEffect, useState } from 'react';
-import { Trash2Icon } from 'lucide-react';
+import { Calendar, Trash2Icon } from 'lucide-react';
 import { Edit2Icon } from 'lucide-react';
 import { InfoIcon } from 'lucide-react';
 import { StarIcon } from 'lucide-react';
@@ -38,16 +38,15 @@ export const ModalActivity = ({ handleClose, deleteActivity, eventInfo }) => {
   return (
     <>
       <div className="flex justify-center items-center fixed top-0 left-0 w-screen h-screen z-50 bg-black/80 cursor-default">
-        <div className="flex flex-col gap-y-2 bg-white p-4 rounded-lg shadow-md text-center w-[90%] max-w-[500px] h-[75vh] overflow-y-auto relative">
-          <div className="flex justify-end gap-x-2 w-full">
+        <div className="flex flex-col justify-evenly md:justify-start gap-y-2 bg-white p-4 rounded-lg shadow-md text-center w-[90%] lg:max-w-[800px] h-[75vh] overflow-y-auto relative">
+          <div className="flex justify-end gap-x-6 w-full">
             <button onClick={() => setIsEditing(true)}>
               <Edit2Icon size={20} color="#49A2EC" />
             </button>
-
             <button onClick={() => deleteActivity(Number(eventInfo.id))}>
               <Trash2Icon size={20} color="#49A2EC" />
             </button>
-            <div className="relative bg-gray-50 hover:bg-gray/25 rounded-full w-10 h-10  flex justify-center items-center">
+            <div className="relative bg-gray-50 hover:bg-gray/25 rounded-full w-10 h-10 flex justify-center items-center">
               <span
                 className="absolute top-0 right-0 left-0 text-[26px] text-center text-gray font-bold cursor-pointer"
                 onClick={handleClose}
@@ -56,14 +55,15 @@ export const ModalActivity = ({ handleClose, deleteActivity, eventInfo }) => {
               </span>
             </div>
           </div>
-
-          <div className="flex gap-x-2 items-center">
-            <div className="bg-primary w-5 h-5 rounded-sm"></div>
-            <span className="text-[11px] md:text-xl font-semibold whitespace-normal break-words text-black">
-              {eventInfo.title.replace(/ - \d{1,2} \w+\./, '')}
-            </span>
-            <span className="flex gap-x-2 text-[11px] md:text-sm font-semibold whitespace-normal break-words text-gray/95 md:ml-7">
-              <StarIcon size={20} color="#49A2EC" fill="#49A2EC" />
+          <div className="flex gap-x-2 items-center justify-between m-4">
+            <div className="flex items-center gap-x-2">
+              <div className="bg-primary w-5 h-5 rounded-sm"></div>
+              <span className="text-lg md:text-2xl font-semibold whitespace-normal break-words text-black">
+                {eventInfo.title.replace(/ - \d{1,2} \w+\./, '')}
+              </span>
+            </div>
+            <span className="flex gap-x-2 text-md md:text-lg font-semibold whitespace-normal break-words text-gray/95">
+              <StarIcon size={25} color="#49A2EC" fill="#49A2EC" />
               {eventInfo.extendedProps.rating}
             </span>
           </div>
@@ -89,31 +89,38 @@ export const ModalActivity = ({ handleClose, deleteActivity, eventInfo }) => {
           {selectedTab === 'info' && (
             <div className="flex flex-col  md:flex-row">
               <div className="flex flex-1 flex-col items-start gap-y-2">
-                <span className="text-[11px] md:text-sm font-semibold whitespace-normal break-words text-black/90 md:ml-7">
-                  {eventInfo.start && eventInfo.start.toLocaleDateString()}{' '}
-                  {eventInfo.start && eventInfo.start.toLocaleTimeString()}{' '}
+                <span className="text-md md:text-lg font-semibold whitespace-normal break-words text-black/90 md:my-3 md:ml-7">
+                  {eventInfo.start && eventInfo.start.toLocaleDateString('en-GB')} -{' '}
+                  {eventInfo.start &&
+                    eventInfo.start.toLocaleTimeString([], {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                    })}{' '}
                 </span>
                 <Link
                   to={`/lugar-esperado/${eventInfo.extendedProps.googleId}`}
-                  className="flex gap-x-2 text-[11px] md:text-sm font-semibold whitespace-normal break-words text-gray/95 hover:text-gray/70 md:ml-7 cursor-pointer"
+                  className="flex items-center gap-x-2 text-md md:text-lg font-semibold whitespace-normal break-words text-gray/95 hover:text-gray/70 md:my-3 md:ml-7 cursor-pointer"
                 >
-                  Sobre este lugar <InfoIcon size={20} color="#49A2EC" />
+                  <InfoIcon size={20} color="#49A2EC" />
+                  Sobre este lugar
                 </Link>
-                <div className="flex flex-col items-start gap-y-2 w-full">
-                  <span className="flex text-[11px] md:text-sm font-semibold whitespace-normal break-words text-gray/95 md:ml-6">
+                <div className="flex flex-col items-start gap-x-2 gap-y-2 w-full">
+                  <span className="flex items-center gap-x-2 text-md md:text-lg font-semibold whitespace-normal break-words text-gray/95 hover:text-gray/70 md:my-3 md:ml-7 cursor-pointer">
+                    {' '}
                     <MapPin size={20} color="#49A2EC" />
-
                     {eventInfo.extendedProps.address}
                   </span>
-
-                  <span className="flex flex-col gap-y-2 text-start text-[11px] md:text-sm font-semibold whitespace-normal break-words text-gray/95 md:ml-7">
-                    Horarios:
-                    {eventInfo.extendedProps.hours.map((hour, index) => (
-                      <span className="whitespace-normal break-words" key={index}>
-                        {hour}
-                      </span>
-                    ))}
-                  </span>
+                  <div className="flex gap-x-2 md:my-3 md:ml-7 text-md md:text-lg font-semibold text-gray/95">
+                    {' '}
+                    <Calendar size={20} color="#49A2EC" />
+                    <div className="flex flex-col items-start">
+                      {eventInfo.extendedProps.hours.map((hour, index) => (
+                        <span className="whitespace-normal break-words" key={index}>
+                          {hour}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -121,7 +128,7 @@ export const ModalActivity = ({ handleClose, deleteActivity, eventInfo }) => {
 
           {/* Sección de Reseñas */}
           {selectedTab === 'reviews' && (
-            <div className="flex-1 overflow-y-auto max-h-[400px] p-4">
+            <div className="flex-1 overflow-y-auto p-4">
               {reviews.map((review, index) => (
                 <div key={index} className="flex flex-col items-start gap-y-2">
                   <div className="flex gap-x-2 items-center">
@@ -130,18 +137,18 @@ export const ModalActivity = ({ handleClose, deleteActivity, eventInfo }) => {
                       alt={review.authorName}
                       className="w-8 h-8 rounded-full"
                     />
-                    <span className="text-[11px] md:text-sm font-semibold whitespace-normal break-words text-black">
+                    <span className="text-md md:text-lg font-semibold whitespace-normal break-words text-black">
                       {review.authorName}
                     </span>
-                    <span className="flex gap-x-2 text-[11px] md:text-sm font-semibold whitespace-normal break-words text-gray/95 md:ml-7">
+                    <span className="flex items-center gap-x-2 text-md md:text-lg font-semibold whitespace-normal break-words text-gray/95 md:my-3 md:ml-7">
                       <StarIcon size={20} color="#49A2EC" fill="#49A2EC" />
                       {review.rating}
                     </span>
                   </div>
-                  <span className="text-[11px] md:text-sm font-semibold whitespace-normal break-words text-gray/95 md:ml-7">
+                  <span className="text-md md:text-lg font-semibold whitespace-normal break-words text-gray/95 md:my-3 md:ml-7">
                     {review.text}
                   </span>
-                  <div className="flex flex-col items-start gap-y-2">
+                  <div className="flex items-center justify-center w-full flex-wrap md:flex-nowrap my-2 gap-2">
                     {review.photos.map((photo: any, index: Key) => (
                       <img
                         key={index}
@@ -151,6 +158,7 @@ export const ModalActivity = ({ handleClose, deleteActivity, eventInfo }) => {
                       />
                     ))}
                   </div>
+                  <hr className="my-2 border-gray w-full" />
                 </div>
               ))}
             </div>
