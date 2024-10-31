67548252a6b2ecbcce37a32f68f753d491e2194e
GonzaSalda <gonza.salda00@gmail.com>
2024-10-31 19:26:34 -0300

    Add event deletion functionality

--- src/pages/ItineraryCalendar.tsx
+++ src/pages/ItineraryCalendar.tsx
@@ -74,6 +74,8 @@ export const ItineraryCalendar = () => {
           setIsAddingActivity={setIsAddingActivity}
           activities={activities}
           setActivities={setActivities}
+          events={events}
+          setEvents={setEvents}
         />
         <main className="order-1 lg:order-2 col-span-1 container mx-auto">
           <div className="flex flex-col h-full mx-4 mb-4 md:mx-0 md:w-full md:p-4">
