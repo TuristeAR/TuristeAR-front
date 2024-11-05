4a93cad18a48bac3d36c807f4715baaf9c9bacf8
TomasAyerbe <tomayerbe@gmail.com>
2024-11-05 20:02:20 -0300

    Replace georef api for fetch province

---
+++ src/utilities/getJsonUrl.tsx
@@ -0,0 +1,54 @@
+export const getJsonUrl = (provinceId: number) => {
+  switch (provinceId) {
+    case 1:
+      return 'https://api.npoint.io/a5c72f64127f879bcfd5/asentamientos';
+    case 2:
+      return 'https://api.npoint.io/9cafdc1851b3a11b532f/asentamientos';
+    case 3:
+      return 'https://api.npoint.io/e7a665676217cf0bc753/asentamientos';
+    case 4:
+      return 'https://api.npoint.io/f1aadd3bbbd4c31e6728/asentamientos';
+    case 5:
+      return 'https://api.npoint.io/76eb544c4832dede6f50/asentamientos';
+    case 6:
+      return 'https://api.npoint.io/7db7b3b0a4255cce6103/asentamientos';
+    case 7:
+      return 'https://api.npoint.io/f739d3ba8baf32f0a1f3/asentamientos';
+    case 8:
+      return 'https://api.npoint.io/548f8dfc13fa5b33a616/asentamientos';
+    case 9:
+      return 'https://api.npoint.io/67efad76cf26f2af4e1d/asentamientos';
+    case 10:
+      return 'https://api.npoint.io/04bc1e66c0e981cbb6f4/asentamientos';
+    case 11:
+      return 'https://api.npoint.io/7af31a3d7a2a8ff11405/asentamientos';
+    case 12:
+      return 'https://api.npoint.io/86c612e18bfa3103605e/asentamientos';
+    case 13:
+      return 'https://api.npoint.io/7db7515ec5b8a1691d91/asentamientos';
+    case 14:
+      return 'https://api.npoint.io/167a5626abb67ecb2c66/asentamientos';
+    case 15:
+      return 'https://api.npoint.io/c37a697d06699f46e7d9/asentamientos';
+    case 16:
+      return 'https://api.npoint.io/d60e3ff2058b9fbb9562/asentamientos';
+    case 17:
+      return 'https://api.npoint.io/12c625824eef19ec4995/asentamientos';
+    case 18:
+      return 'https://api.npoint.io/b9f91001468ba18a8f1e/asentamientos';
+    case 19:
+      return 'https://api.npoint.io/1fe507b0f001d9f20022/asentamientos';
+    case 20:
+      return 'https://api.npoint.io/8d9ccb029dbdd1aed045/asentamientos';
+    case 21:
+      return 'https://api.npoint.io/c7960d0c403236d4dd57/asentamientos';
+    case 22:
+      return 'https://api.npoint.io/d174a05a69ba52c77326/asentamientos';
+    case 23:
+      return 'https://api.npoint.io/de2f4d20817486420d81/asentamientos';
+    case 24:
+      return 'https://api.npoint.io/29f70d7bf495bad861f0/asentamientos';
+    default:
+      return '';
+  }
+};
