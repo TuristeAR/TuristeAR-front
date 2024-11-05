15b7b606fd38ba3d9fc62664442a0b988c29f436
GonzaSalda <gonza.salda00@gmail.com>
2024-11-04 23:21:27 -0300

    Add Swiper carousel to display events

--- src/pages/FormQuestions.tsx
+++ src/pages/FormQuestions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Header } from '../components/Header/Header';
 import { MapaArg } from '../components/Destinations/MapaArg';
 import { ProgressBar } from '../components/Questions/ProgressBar';
@@ -8,6 +8,8 @@ import { DateRangePicker } from 'react-date-range';
 import { es } from 'date-fns/locale';
 import { useNavigate } from 'react-router-dom';
 import { get, getWithoutCredentials, post } from '../utilities/http.util';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import {
   Baby,
   Banknote,
@@ -29,6 +31,7 @@ import Lottie from 'lottie-react';
 import logoAnimado from '../assets/logoAnimado.json';
 import EventCarousel from '../components/FormQuestions/EventCarousel';
 import formatFromDateAndToDate from '../utilities/formatEventDate';
+import { EventCard } from '../components/ItineraryCalendar/EventCard';
 
 interface FormData {
   provinceId: number;
@@ -475,6 +478,18 @@ const FormQuestions = () => {
     fetchSession();
   }, []);
 
+  const [carouselEvents, setCarouselEvents] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (selectedProvince?.id) {
+      fetchEvents(selectedProvince.id).then((data) => {
+        setCarouselEvents(data.data);
+      });
+    }
+  }, [selectedProvince]);
+
+  const swiperRef = useRef(null);
+
   return (
     <>
       <Header />
@@ -485,6 +500,31 @@ const FormQuestions = () => {
             Estamos armando el viaje ideal para vos...
           </h2>
           <Lottie className="w-[16rem] md:w-[18rem] mx-auto" animationData={logoAnimado} />
+          <Swiper
+            ref={swiperRef}
+            className="w-full max-w-[90%] sm:max-w-[500px] md:max-w-[600px] lg:max-w-[650px] xl:max-w-[750px]"
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            slidesPerView={1}
+            slidesPerGroup={1}
+            autoplay={{ delay: 2000, disableOnInteraction: false }}
+            loop={true}
+          >
+            {carouselEvents.map((event) => (
+              <SwiperSlide key={event.id}>
+                <EventCard
+                  id={event.id}
+                  fromDate={event.fromDate}
+                  toDate={event.toDate}
+                  name={event.name}
+                  locality={event.locality}
+                  description={event.description}
+                  image={event.image}
+                  isSelected={selectedEvents.includes(event.id)}
+                  onSelect={handleEventSelect}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       ) : (
         <section
