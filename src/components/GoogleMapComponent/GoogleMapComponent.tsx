3d671e37026c3bff24dc2ed809d9742983115afa
TomasAyerbe <tomayerbe@gmail.com>
2024-11-05 21:21:14 -0300

    Update google api key

--- src/components/GoogleMapComponent/GoogleMapComponent.tsx
+++ src/components/GoogleMapComponent/GoogleMapComponent.tsx
@@ -15,7 +15,7 @@ const GoogleMapComponent = ({ latitud, longitud, nombre }) => {
   };
 
   return (
-    <LoadScript googleMapsApiKey={'AIzaSyAwUPoi1TvgbcP2HrIXWF-Gxjalmzke4Ek'}>
+    <LoadScript googleMapsApiKey={'AIzaSyCndlOUUXZiAKyaS4FtLNdIkeZnwPfe2Jc'}>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         center={center}
