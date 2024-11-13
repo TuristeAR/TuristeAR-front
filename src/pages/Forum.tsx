bb2954b7d6cbb942b6f9e5404536bb3a26dc7396
Paolo Aleman <paoloaleman86@gmail.com>
2024-11-12 23:33:13 -0300

    Refactor to ItineraryCard and add seeker to Forums

--- src/pages/Forum.tsx
+++ src/pages/Forum.tsx
@@ -24,6 +24,7 @@ const Forum = () => {
   const [categorySelected, setCategorySelected] = useState<number | null>(null);
   const [forums, setForums] = useState<Forum[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [description, setDescription] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -71,42 +72,60 @@ const Forum = () => {
               setIsOpen={setIsOpen}
             />
             <div
-              className="absolute md:static lg:w-[80%] w-[100%] py-10 flex flex-col gap-10 ${
+              className="absolute md:static lg:w-[80%] w-[100%] pb-10 pt-6 flex flex-col gap-10 ${
                 scrollbar-hidden"
             >
+              <div className={'w-[97%] mx-auto flex flex-col gap-4'}>
+                <h2 className="text-xl font-bold">Buscar foro</h2>
+                <form className="flex flex-col gap-4">
+                  <input
+                    type="text"
+                    onInput={(e) =>
+                      setDescription(
+                        //@ts-ignore
+                        e.target.value,
+                      )
+                    }
+                    className="border border-[#999999] pl-2 w-[45%]"
+                    placeholder="Buscar"
+                    autoComplete="off"
+                  />
+                </form>
+              </div>
               <CreateForums />
               <div className={'grid lg:grid-cols-2 grid-cols-1 mx-4 gap-6'}>
-                {forums
-                  ?.filter((forum) => {
-                    return categorySelected == null || forum.category.id == categorySelected;
-                  }).length > 0 ?
-                  (forums
-                  ?.filter((forum) => {
-                    return categorySelected == null || forum.category.id == categorySelected;
-                  })
-                  .map((forum, index) => (
-                    <div
-                      key={index}
-                      className="w-[100%] mx-auto h-fit p-4 rounded-2xl shadow-[0_10px_25px_-10px_rgba(0,0,0,4)] flex flex-col gap-4"
-                    >
-                      <h1 className={'text-2xl'}>{forum.name}</h1>
-                      <p>{forum.description}</p>
-                      <div className={'flex justify-between items-center h-full'}>
-                        <div className="flex items-center gap-2">
-                          <MapPin name="info" />
-                          <span className="text-sm text-gray">{forum.category.description}</span>
-                        </div>
-                        <div
-                          className="lg:btn-blue px-4 py-2 bg-primary hover:bg-primary-3 text-white rounded-2xl flex items-center justify-center">
-                          <a href={`/forum/${forum.id}`}>Ingresar</a>
+                {forums?.filter((forum) => {
+                  return (categorySelected == null || forum.category.id == categorySelected) && (description == null || forum.description.toLowerCase().includes(description.toLowerCase()) || forum.name.toLowerCase().includes(description.toLowerCase())  );
+                }).length > 0 ? (
+                  forums
+                    ?.filter((forum) => {
+                      return (categorySelected == null || forum.category.id == categorySelected) && (description == null || forum.description.toLowerCase().includes(description.toLowerCase()) || forum.name.toLowerCase().includes(description.toLowerCase()) );
+                    })
+                    .map((forum, index) => (
+                      <div
+                        key={index}
+                        className="w-[100%] mx-auto h-fit p-4 rounded-2xl shadow-[0_10px_25px_-10px_rgba(0,0,0,4)] flex flex-col gap-4"
+                      >
+                        <h1 className={'text-2xl'}>{forum.name}</h1>
+                        <p>{forum.description}</p>
+                        <div className={'flex justify-between items-center h-full'}>
+                          <div className="flex items-center gap-2">
+                            <MapPin name="info" />
+                            <span className="text-sm text-gray">{forum.category.description}</span>
+                          </div>
+                          <div className="lg:btn-blue px-4 py-2 bg-primary hover:bg-primary-3 text-white rounded-2xl flex items-center justify-center">
+                            <a href={`/forum/${forum.id}`}>Ingresar</a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ))): (
-                    <div className={'p-4 bg-[#c0daeb] rounded-2xl'}>
-                      <p className={'text-3xl font-bold'}>No hay foros en la categoría seleccionada!</p>
-                    </div>
-                  )}
+                    ))
+                ) : (
+                  <div className={'p-4 bg-[#c0daeb] rounded-2xl col-span-2'}>
+                    <p className={'text-3xl font-bold'}>
+                      No hay foros en la categoría seleccionada!
+                    </p>
+                  </div>
+                )}
               </div>
             </div>
           </div>
